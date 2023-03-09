39855a3c3de5e7bf184dd4146ebeffbd445c5805
benpfaff23 <126900906+benpfaff23@users.noreply.github.com>
2023-03-09 12:03:11 -0800

    Add files via upload

---
+++ MyGame/settings.py
@@ -0,0 +1,18 @@
+# File created by Ben Pfaff
+
+# screen color  
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+RUNNING = True
+
+# player attributes 
+PLAYER_ACC = 2 
+PLAYER_FRICTION = -0.12
+
